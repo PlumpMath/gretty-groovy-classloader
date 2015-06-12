1df96fdf2befebe045ea24bd64954758589d5dae
Kevin Page <kep@goldengekko.com>
2015-06-12 10:53:31 +0700

    Example app showing gretty groovy classloader issue

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import javax.sql.DataSource;
+
+import liquibase.integration.spring.SpringLiquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+    static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public DataSource myDataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase lb = new SpringLiquibase();
+        lb.setChangeLog("classpath:changelog.groovy");
+        lb.setContexts("development, test, production");
+        lb.setDataSource(myDataSource());
+        return lb;
+    }
+}
